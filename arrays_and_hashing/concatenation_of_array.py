480df822ade010f136980c0d19b412d128629806
Sohil Chanana <schanana2003@gmail.com>
2025-09-25 00:48:40 -0400

    completed 'concat of array' and 'longest common prefix'

---
+++ arrays_and_hashing/concatenation_of_array.py
@@ -0,0 +1,10 @@
+# Leetcodde Problem: 1929. Concatenation of Array
+# https://leetcode.com/problems/concatenation-of-array/
+
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return nums + nums
---
+++ arrays_and_hashing/longest_common_prefix.py
@@ -0,0 +1,14 @@
+# Leetcode Problem: 14. Longest Common Prefix
+# https://leetcode.com/problems/longest-common-prefix/
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return s[:i]
+        return strs[0]
