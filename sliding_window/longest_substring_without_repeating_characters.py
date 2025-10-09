f1318a504369f59286f53c20917a55c0c725a327
Sohil Chanana <schanana2003@gmail.com>
2025-10-08 23:32:54 -0400

    completed longest substring without repeating characters

---
+++ sliding_window/longest_substring_without_repeating_characters.py
@@ -0,0 +1,26 @@
+# Leetocde Problem 3: Longest Substring Without Repeating Characters
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        maxLen = 0
+        seen = set()
+        l = 0
+
+        for r in range(len(s)):
+            while s[r] in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(s[r])
+            maxLen = max(maxLen, r - l + 1)
+        return maxLen
+
+
+        
+
+
+        
